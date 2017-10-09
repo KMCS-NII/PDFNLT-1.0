5e06087d757c7a759301b8d88a185117ea73f925
shinochin <kazutoshi,shinoda0516@gmail.com>
2017-10-09 18:26:19 +0900

    Add Main.py

---
+++ preprocess/Main.py
@@ -0,0 +1,16 @@
+import argparse
+from Preprocess import PathFiles
+from config import PATH_TO_SOURCE, PATH_TO_OUTPUT
+
+def main():
+    parser = argparse.ArgumentParser(description='Preprocess papers')
+    parser.add_argument('--limit', '-l', type=int, default=None,
+                        help='Indicate limit of processing files if you want')
+    args = parser.parse_args()
+    
+    files = PathFiles(PATH_TO_SOURCE, limit=args.limit)
+    files.app_preprocess()
+    
+if __name__=='__main__':
+    
+    main()
