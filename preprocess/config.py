e82544a6ad191ad8b16735a1f6b05c152dafb002
shinochin <kazutoshi,shinoda0516@gmail.com>
2017-10-09 15:19:14 +0900

    Add config.py

---
+++ preprocess/config.py
@@ -0,0 +1,9 @@
+#
+#入力・出力ファイルのパスを管理
+#
+
+ROOT = "/works/csisv12/akiko/acl_anthology/"
+
+
+PATH_TO_FLIES = ROOT + "SENT.tsv.out.selected/"
+PATH_TO_OUTPUT = ROOT + "SENT.tsv.out.selected.preprocess/"
