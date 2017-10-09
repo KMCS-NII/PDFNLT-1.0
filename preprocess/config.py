fd23e9088b907c73114db350bcefd4677ddda462
shinochin <kazutoshi,shinoda0516@gmail.com>
2017-10-09 16:30:29 +0900

    Update config.py

--- preprocess/config.py
+++ preprocess/config.py
@@ -5,5 +5,5 @@
 ROOT = "/works/csisv12/akiko/acl_anthology/"
 
 
-PATH_TO_FLIES = ROOT + "SENT.tsv.out.selected/"
+PATH_TO_SOURCE = ROOT + "SENT.tsv.out.selected/"
 PATH_TO_OUTPUT = ROOT + "SENT.tsv.out.selected.preprocess/"
