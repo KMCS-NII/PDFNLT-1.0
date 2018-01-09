1dd7da06cc875bd6ed0b1bfed5cb0f8ca3ade47d
shinochin <kazutoshi.shinoda0516@gmail.com>
2018-01-09 16:06:25 +0900

    preprocess -> nlp

--- preprocess/config.py
+++
@@ -1,9 +0,0 @@
-#
-#入力・出力ファイルのパスを管理
-#
-
-ROOT = "/works/csisv12/akiko/acl_anthology/"
-
-
-PATH_TO_SOURCE = ROOT + "SENT.tsv.out.selected/"
-PATH_TO_OUTPUT = ROOT + "SENT.tsv.out.selected.preprocess/"
